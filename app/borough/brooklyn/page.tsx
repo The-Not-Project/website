512d76f1ef07cea08725b1bf99993b35da33561a
Tarriq <tarekelghayate@gmail.com>
2025-01-15 23:39:35 -0500

    initial commit

---
+++ app/borough/brooklyn/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page() {
+    return (
+        <main>
+            <h1><Link href="/">Home</Link></h1>
+        <h1>Brooklyn</h1>
+        </main>
+    );
+}
---
+++ app/borough/queens/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page() {
+    return (
+        <main>
+            <h1><Link href="/">Home</Link></h1>
+        <h2>Queens</h2>
+        </main>
+    );
+}
