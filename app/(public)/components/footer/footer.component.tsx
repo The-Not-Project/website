e7e129a13219b3ec8e3b694ffc69f0afe083f3e9
Sebastiantorrescodes <sebastian.torres.codes@gmail.com>
2025-04-29 17:13:06 -0400

    Refactor Footer component for improved structure and styling, enhance social media links, and update sign-up form layout

--- app/(public)/components/footer/footer.component.tsx
+++ app/(public)/components/footer/footer.component.tsx
@@ -5,123 +5,118 @@ import Image from "next/image";
 import { useStore } from "@/app/zustand/store";
 
 import {
-  FooterContainer,
+  Footer,
   MainContent,
-  Instagram,
   NavSection,
   SignUpSection,
   SignUpForm,
   SignUp,
   ConsentText,
   BottomBar,
-  SocialLinks,
   LegalLinks,
+  Instagram,
+  Youtube,
+  X,
   Copyright,
-  Footer,
 } from "./footer.styles";
 import clsx from "clsx";
 
 export default function FooterComponent() {
-
   const isMenuOpen = useStore((state) => state.mobileLayout.isMenuOpen);
 
   return (
-    <FooterContainer className={clsx('page-wrapper', { shifted: isMenuOpen })}>
-      <Footer>
-        <MainContent>
-          <div className="logoSection">
-            <Image
-              src="/media/logo.png"
-              alt="The Not Project Logo"
-              width={120}
-              height={68}
-            />
-          </div>
+    <Footer className={clsx("page-wrapper", { shifted: isMenuOpen })}>
+      <MainContent>
+        <div className="logoSection">
+          <Image
+            src="/media/logo.png"
+            alt="The Not Project Logo"
+            width={120}
+            height={68}
+          />
+        </div>
+        <NavSection>
+          <h2>Company</h2>
+          <ul>
+            <li>
+              <Link href="/contact">Contact Us</Link>
+            </li>
+            <li>
+              <Link href="/collaborations">Collaborations</Link>
+            </li>
+            <li>
+              <Link href="/faq">FAQ</Link>
+            </li>
+          </ul>
+        </NavSection>
 
-          <NavSection>
-            <h2>Company</h2>
-            <ul>
-              <li>
-                <Link href="/publications">Publications</Link>
-              </li>
-              <li>
-                <Link href="/mission">Mission</Link>
-              </li>
-              <li>
-                <Link href="/members">Members</Link>
-              </li>
-              <li>
-                <Link href="/story">Story</Link>
-              </li>
-            </ul>
-          </NavSection>
+        <NavSection>
+          <h2>Our Socials</h2>
+          <ul>
+            <li>
+              <a
+                href="https://www.instagram.com/_thenotproject"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Instagram"
+              >
+                <Instagram />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.youtube.com/@thenotproject"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Youtube"
+              >
+                <Youtube />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://x.com/TheNotProject"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="X"
+              >
+                <X />
+              </a>
+            </li>
+          </ul>
+        </NavSection>
 
-          <NavSection>
-            <h2>Support</h2>
-            <ul>
-              <li>
-                <Link href="/contact">Contact Us</Link>
-              </li>
-              <li>
-                <Link href="/careers">Careers</Link>
-              </li>
-              <li>
-                <Link href="/instagram">Instagram</Link>
-              </li>
-              <li>
-                <Link href="/faq">FAQ</Link>
-              </li>
-            </ul>
-          </NavSection>
+        <SignUpSection>
+          <h2>Never Miss Out</h2>
+          <SignUpForm>
+            <p>Be the first to know about our new stories.</p>
+            <SignUp>
+              <input type="email" placeholder="Enter your Email Address" />
+              <input type="tel" placeholder="Phone Number" />
+              <button type="submit">Sign Up</button>
+            </SignUp>
+          </SignUpForm>
+          <ConsentText>
+            By entering your phone number and submitting this form, you consent
+            to receive marketing text messages from The Not Project at the
+            number provided, including messages sent by autodialer. Message and
+            data rates may apply. Message frequency varies. You can unsubscribe
+            at any time by replying STOP or clicking the unsubscribe link (where
+            available) in one of our messages. View our{" "}
+            <Link href="/privacy">Privacy Policies</Link> and{" "}
+            <Link href="/terms">Terms & Conditions</Link>.
+          </ConsentText>
+        </SignUpSection>
+      </MainContent>
 
-          <SignUpSection>
-            <h2>Never Miss Out</h2>
-            <SignUpForm>
-              <p>Be the first to know about our new stories</p>
-              <SignUp>
-                <input type="email" placeholder="Email Address" />
-                <input type="tel" placeholder="Phone Number" />
-                <button type="submit">SIGN UP</button>
-              </SignUp>
-            </SignUpForm>
-            <ConsentText>
-              By entering your phone number and submitting this form, you
-              consent to receive marketing text messages from The Not Project at
-              the number provided, including messages sent by auto dialer.
-              Message and data rates may apply. Message frequency varies. You
-              can unsubscribe at any time by replying STOP or clicking the
-              unsubscribe link (where available) in one of our messages. View
-              our <Link href="/privacy">Privacy Policies</Link> and{" "}
-              <Link href="/terms">Terms & Conditions</Link>.
-            </ConsentText>
-          </SignUpSection>
-        </MainContent>
-
-        <BottomBar>
-          <SocialLinks>
-            <Link href="/instagram" aria-label="Instagram">
-              <Instagram />
-            </Link>
-            {/* <Link href="/youtube" aria-label="YouTube">
-              <Youtube />
-            </Link> */}
-
-            {/* <a
-              href="https://www.youtube.com/@TheNotProject"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="YouTube"
-            >
-              <Youtube />
-            </a> */}
-          </SocialLinks>
-          <LegalLinks>
-            <Link href="/privacy">Privacy Policies</Link>
-            <Link href="/terms">Terms & Conditions</Link>
-          </LegalLinks>
-          <Copyright>The Not Project 2025</Copyright>
-        </BottomBar>
-      </Footer>
-    </FooterContainer>
+      <BottomBar>
+        <Copyright>© The Not Project</Copyright>
+        <LegalLinks>
+          <Link href="/privacy">Privacy Policies</Link>
+          <Link href="/terms">Terms & Conditions</Link>
+        </LegalLinks>
+        <Copyright>2025</Copyright>
+      </BottomBar>
+    </Footer>
   );
 }
